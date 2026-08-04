825e1d93d1e5b4dc7e3d9aeae5b406e844c0a011
Emmanuel Matson <emmanuelmatson8@gmail.com>
2026-08-04 21:58:33 +0300

    Hidden bug fixed for the toggle

--- portfolio/src/layouts/Navbar.jsx
+++ portfolio/src/layouts/Navbar.jsx
@@ -30,7 +30,7 @@ function Navbar() {
   }, []);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 w-full bg-slate-900 shadow-lg z-50 overflow-x-hidden">
+    <nav className="fixed top-0 left-0 right-0 w-full bg-slate-900 shadow-lg z-50">
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
 
