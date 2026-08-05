f294217d9efe6eaaa4e9b2aa84ace6ad1c6aa6c7
Emmanuel Matson <emmanuelmatson8@gmail.com>
2026-08-05 21:20:26 +0300

    logo updated

--- portfolio/src/components/common/Logo.jsx
+++ portfolio/src/components/common/Logo.jsx
@@ -1,26 +1,16 @@
+
+import logo from "../../assets/logos/em-logo.png";
+
 function Logo() {
   return (
-    <div
-      className="
-        flex
-        items-center
-        font-black
-        tracking-tight
-        select-none
-        whitespace-nowrap
-        text-3xl
-        sm:text-4xl
-      "
-    >
-      <span className="text-cyan-400">&lt;</span>
-
-      <span className="text-white">
-        EM
-      </span>
-
-      <span className="text-cyan-400">/&gt;</span>
-    </div>
+    
+    <img
+      src={logo}
+      alt="Emmanuel Matson Logo"
+      className="h-14 w-auto"
+    />
   );
+  
 }
 
 export default Logo;
